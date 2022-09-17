f01eb8eec975c0a3d11cba2c7878192672f4972c
gustavofamorim <gustavofreitasamorim@gmail.com>
2022-09-17 17:27:29 +0100

    6555107

---
+++ precache-manifest.12bd4c3ac1201eb0e0f58062148e84b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746548455268db30c3e2",
+    "url": "/css/app.996e3eae.css"
+  },
+  {
+    "revision": "25f22211691b392bf5c7e29f849b60f7",
+    "url": "/google9fec050df6d16a74.html"
+  },
+  {
+    "revision": "e3037a45cf9ffc4a5f39cd2eb9ec4dad",
+    "url": "/img/me.jpg"
+  },
+  {
+    "revision": "766ee80b026583c147f9be9cb5a8edea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746548455268db30c3e2",
+    "url": "/js/app.2d189ed0.js"
+  },
+  {
+    "revision": "4961fe28a3445cfdd664",
+    "url": "/js/chunk-vendors.69cece72.js"
+  },
+  {
+    "revision": "f044a0cee1a89ea6696954f6ff3bb188",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
